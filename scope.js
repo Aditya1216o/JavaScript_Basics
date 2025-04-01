327ad35c0e368dd104c9c248f67162047540a36d
aditya desai <addityadesai00034@gmail.com>
2025-04-01 22:02:34 +0530

    hoisting

--- scope.js
+++ scope.js
@@ -1,8 +1,45 @@
-let a = 10
-const b =20
-var c=30
+// let a = 10
+// const b =20
+// var c=30
 
 
-console.log("A"-1);
-console.log("tilak"+"A");
-console.log(c);
+// console.log("A"-1);
+// console.log("tilak"+"A");
+// console.log(c);
+function one () {
+    const username = "hitesh"
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+
+    }
+    // console.log(website);
+    two() 
+
+    
+}
+one()
+
+if (true ) {
+    const username = "hitesh"
+    if(username ===  "hitesh"){
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+addone(5)
+function addone(num) {
+    return num + 1
+}
+
+
+
+const addtwo = function(num) {
+    return num+2
+}
+addtwo(5)
