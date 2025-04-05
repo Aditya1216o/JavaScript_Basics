0647388e25c9c5ef3a2c84c6dbde50f722197d57
aditya desai <addityadesai00034@gmail.com>
2025-04-05 11:12:19 +0530

    updated

---
+++ control_flow/one.js
@@ -0,0 +1,53 @@
+//  if (2 == "2") {
+//     console.log("executed");
+    
+//  }
+
+ // < , >, <= , >= ,== , != , === , !==
+
+//  const temp =41 
+//  if(temp === 41) {
+    // console.log("less than 50 ");
+    
+//  } else{
+    // console.log("tempertature is greater than 50")
+//  }
+ 
+//  const score = 200 
+//  if (score > 300) {
+//     const power = "fly"
+//     console.log(`user power:${power}`);
+    
+//  }
+// const balance = 1000
+// if(balance > 500 ) console.log("test"),console.log("test2");
+
+// if (balance > 500) {
+//     console.log("less than 500");
+    
+// }else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if (balance < 900) {
+//     console.log("less than 750");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+const userloggedIn =true
+const debitcard = true
+const loggedinfromgoggle = false
+const loggedfromemail = true
+
+if (userloggedIn && debitcard && 2==3) {
+    console.log("allow to buy courses");
+    
+}
+
+if (loggedfromemail || loggedinfromgoggle) {
+    console.log("user logged in" );
+    
+}
+ 
---
+++ control_flow/switch.js
@@ -0,0 +1,27 @@
+const month = 3 
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("default month matched ");
+        
+        break;
+}
+
+// truthy values 
+//"0", 'false', " ", [], {}, function{}
+
+//falsy values
+// false, 0, -0, BigInt 0n,"", null, undefined, NaN
